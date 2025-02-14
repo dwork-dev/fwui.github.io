dcb0b9715d5eca305788bee25793b6c0c4db19e3
dipgle <qvt0484@gmail.com>
2025-02-14 16:00:45 +0700

    Update sdk.js

--- dist-beta/extens/sdk.js
+++ dist-beta/extens/sdk.js
@@ -1095,4 +1095,270 @@ function clone(obj){
   }
   return obj;
 }
+function initDrag(element,options) {
+  options=options||{};
+  var zone=options.zone||element;
+  var area=options.area||document.body;
+  var mouseTop,mouseLeft,posTop,posLeft,newTop,newLeft,widthMove,heightMove;
+  var position = element.style.position||"fixed";
+  var timeout=2000;
+  zone.onmousedown = onmousedown;
+  element.style.width=element.offsetWidth+"px";
+  function onmousedown(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    mouseTop=e.y;
+    mouseLeft=e.x;
+    widthMove=0;
+    heightMove=0;
+    posTop = element.getBoundingClientRect().y;
+    posLeft = element.getBoundingClientRect().x;
+    newTop=posTop;
+    newLeft=posLeft;
+    //var zoneOffset = element.getBoundingClientRect();
+    //console.log("e",e)
+
+    element.style.top=(newTop)+"px";
+    element.style.left=(newLeft)+"px";
+    element.style.position = "fixed";
+    document.onmouseup = onmouseup;
+    document.onmousemove = onmousemove;
+    document.scrollDown = scrollDown;
+    document.scrollDown();
+    typeof options.onStart == "function" && options.onStart();
+  }
+  function scrollDown(){
+    var speed=Math.abs(element.down||0)||1;
+    if(speed>7){
+      area.scrollTop += element.down;
+    }
+    speed=speed/10;
+    if(speed>20){
+      speed==20;
+    }
+    setTimeout(()=>{
+      if(typeof document.scrollDown=="function"){
+        document.scrollDown();
+      }
+    }, 50)
+  }
+  function onmousemove(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    widthMove=mouseTop-e.y;
+    heightMove=mouseLeft-e.x;
+    newTop=posTop-(widthMove);
+    newLeft=posLeft-(heightMove);
+    var mint=0+area.getBoundingClientRect().y;
+    var minl=0+area.getBoundingClientRect().x;
+    var maxt=mint+area.offsetHeight-element.offsetHeight;
+    var maxl=minl+area.offsetWidth-element.offsetWidth;
+    if(maxt>screen.height-element.offsetHeight){
+      maxt=screen.height-element.offsetHeight;
+    }
+    if(maxl>screen.width-element.offsetWidth){
+      maxl=screen.width-element.offsetWidth;
+    }
+    if(newLeft<minl){
+      newLeft=minl;
+    }
+    if(newLeft>maxl){
+      newLeft=maxl;
+    }
+    element.down=0;
+    if(newTop<mint){
+      element.down=newTop-mint;
+      newTop=mint;
+    }
+    if(newTop>maxt){
+      element.down=newTop-maxt;
+      newTop=maxt;
+    }
+    element.style.top = newTop + "px";
+    element.style.left = newLeft + "px";
+    //console.log("down",element.down)
+    typeof options.onDrag == "function" && options.onDrag();
+  }
+
+  function onmouseup(e) {
+    element.style.position = position;
+    element.style.top = (newTop-area.getBoundingClientRect().y+area.scrollTop) + "px";
+    element.style.left = (newLeft-area.getBoundingClientRect().x+area.scrollLeft) + "px";
+    document.onmouseup = null;
+    document.onmousemove = null;
+    document.scrollDown = null;
+    typeof options.onDrop == "function" && options.onDrop();
+  }
+  function getData() {
+    return {
+      top: element.offsetTop,
+      left: element.offsetLeft,
+      width: element.offsetWidth,
+      height: element.offsetHeight
+    };
+  }
+}
+function initResize(el,options){
+  options=options||{};
+  if(!el.elmove){
+    el.elmove = document.createElement("div");
+    document.body.append(el.elmove);
+    el.elmove.setAttribute("style","position:fixed;display:none");
+    el.elmove.innerHTML=`
+      <div style="position:relative;width:20px;height:20px;">
+      	<div style="position:absolute; border-bottom:3px solid #bbb; border-top:1px solid #fff; bottom:5px; right:5px; width:12px; height:0px; cursor:nwse-resize;"></div>
+      	<div style="position:absolute; border-right:3px solid #bbb; border-left:1px solid #fff; bottom:5px;right:5px; height:12px; width:0px; cursor:nwse-resize"></div>
+      </div>
+      `;
+    el.addEventListener("mouseover",e=>{
+      el.style.background = "#ccc";
+    });
+    el.addEventListener("mouseleave",e=>{
+      //el.style.background = "";
+    });
+    el.elmove.onmouseover=()=>{
+      el.elmove.style.display = "";
+    }
+    el.elmove.onmouseleave=()=>{
+      el.elmove.style.display = "none";
+      el.style.border = "";
+    }
+    initDragDrop(el.elmove,{
+      onStart: ()=>{
+        el.sizes = {
+          x: el.elmove.getBoundingClientRect().x,
+          y: el.elmove.getBoundingClientRect().y,
+          w: el.offsetWidth,
+          h: el.offsetHeight,
+        }
+        console.log("onStart",el.elmove.sizes)
+      },
+      onDrop: ()=>{
+
+      },
+      onDrag: ()=>{
+        el.style.width=(el.sizes.w+(el.elmove.getBoundingClientRect().x - el.sizes.x - 10))+"px";
+        el.style.height=(el.sizes.h+(el.elmove.getBoundingClientRect().y - el.sizes.y - 10))+"px";
+        //console.log("onDrag",event)
+      }
+    })
+  }
+
+  el.addEventListener("mousemove",e=>{
+    el.elmove.style.display = "";
+    el.elmove.style.top = (el.getBoundingClientRect().y+el.getBoundingClientRect().height-15)+"px";
+    el.elmove.style.left = (el.getBoundingClientRect().x+el.getBoundingClientRect().width-15)+"px";
+  });
+  el.addEventListener("mouseleave",e=>{
+    el.elmove.style.display = "none";
+  });
+}
+function initDrag(element,options) {
+  options=options||{};
+  var zone=options.zone||element;
+  var area=options.area||document.body;
+  var mouseTop,mouseLeft,posTop,posLeft,newTop,newLeft,widthMove,heightMove;
+  var position = element.style.position||"fixed";
+  var timeout=2000;
+  zone.onmousedown = onmousedown;
+  element.style.width=element.offsetWidth+"px";
+  function onmousedown(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    mouseTop=e.y;
+    mouseLeft=e.x;
+    widthMove=0;
+    heightMove=0;
+    posTop = element.getBoundingClientRect().y;
+    posLeft = element.getBoundingClientRect().x;
+    newTop=posTop;
+    newLeft=posLeft;
+    //var zoneOffset = element.getBoundingClientRect();
+    //console.log("e",e)
+    
+    element.style.top=(newTop)+"px";
+    element.style.left=(newLeft)+"px";
+    element.style.position = "fixed";
+    document.onmouseup = onmouseup;
+    document.onmousemove = onmousemove;
+    document.scrollDown = scrollDown;
+    document.scrollDown();
+    typeof options.onStart == "function" && options.onStart();
+  }
+  function scrollDown(){
+    var speed=Math.abs(element.down||0)||1;
+    if(speed>7){
+      area.scrollTop += element.down;
+    }
+    speed=speed/10;
+    if(speed>20){
+      speed==20;
+    }
+    setTimeout(()=>{
+      if(typeof document.scrollDown=="function"){
+        document.scrollDown();
+      }
+    }, 50)
+  }
+  function onmousemove(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    widthMove=mouseTop-e.y;
+    heightMove=mouseLeft-e.x;
+    newTop=posTop-(widthMove);
+    newLeft=posLeft-(heightMove);
+    var mint=0+area.getBoundingClientRect().y;
+    var minl=0+area.getBoundingClientRect().x;
+    var maxt=mint+area.offsetHeight-element.offsetHeight;
+    var maxl=minl+area.offsetWidth-element.offsetWidth;
+    if(maxt>screen.height-element.offsetHeight){
+      maxt=screen.height-element.offsetHeight;
+    }
+    if(maxl>screen.width-element.offsetWidth){
+      maxl=screen.width-element.offsetWidth;
+    }
+    if(newLeft<minl){
+      newLeft=minl;
+    }
+    if(newLeft>maxl){
+      newLeft=maxl;
+    }
+    element.down=0;
+    if(newTop<mint){
+      element.down=newTop-mint;
+      newTop=mint;
+    }
+    if(newTop>maxt){
+      element.down=newTop-maxt;
+      newTop=maxt;
+    }
+    element.style.top = newTop + "px";
+    element.style.left = newLeft + "px";
+    //console.log("down",element.down)
+    typeof options.onDrag == "function" && options.onDrag();
+  }
+
+  function onmouseup(e) {
+    element.style.position = position;
+    element.style.top = (newTop-area.getBoundingClientRect().y+area.scrollTop) + "px";
+    element.style.left = (newLeft-area.getBoundingClientRect().x+area.scrollLeft) + "px";
+    document.onmouseup = null;
+    document.onmousemove = null;
+    document.scrollDown = null;
+    typeof options.onDrop == "function" && options.onDrop();
+  }
+  function getData() {
+    return {
+      top: element.offsetTop,
+      left: element.offsetLeft,
+      width: element.offsetWidth,
+      height: element.offsetHeight
+    };
+  }
+}
+
 
